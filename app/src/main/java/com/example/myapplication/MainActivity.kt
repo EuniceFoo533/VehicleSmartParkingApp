2d7096eba947de09d838dc069f39da647b144bff
EuniceFoo533 <b032110050@student.utem.edu.my>
2023-04-15 23:31:09 +0800

    Vehicle Smart Parking App

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var login :Button = findViewById(R.id.button)
+
+        var editEmail :EditText =findViewById(R.id.editEmail)
+        var editPassword :EditText = findViewById(R.id.editPassword)
+
+        login.setOnClickListener{
+            if(editEmail.text.isNullOrBlank() && editPassword.text.isNullOrBlank())
+            {
+                Toast.makeText(this,"Please fill in the details" , Toast.LENGTH_SHORT).show()
+            }
+
+            else
+            {
+             Toast.makeText(this,"${editEmail.text} is login", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
