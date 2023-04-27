b274bb258908c311b68041bad6f72f051e5e4684
EuniceFoo533 <b032110050@student.utem.edu.my>
2023-04-27 23:42:39 +0800

    Vehicle Smart Parking App

--- app/src/main/java/com/example/myapplication/SplashActivity.kt
+++ app/src/main/java/com/example/myapplication/SplashActivity.kt
@@ -20,7 +20,7 @@ class SplashActivity : AppCompatActivity() {
         )
 
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, LoginActivity::class.java)
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }, 3000) // 3000 is the delayed time in milliseconds.
